b75db3592eeae3958a80e2594949bf16c615a722
Ben Hamelin <bhamelin@oomphinc.com>
2020-08-13 07:05:32 -0400

    Add default lando settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -770,14 +770,62 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
-  'database' => 'drupal9',
-  'username' => 'drupal9',
-  'password' => 'drupal9',
-  'prefix' => '',
-  'host' => 'database.drupalscaffold.internal',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['config_sync_directory'] = 'sites/default/files/config_rAarLX2gR_-gVA4biGiJA6FZRoHwNRuKOfjoS-jpx0t55xFpm6MnxxoDHRf52QA3_4epQZqqyg/sync';
+
+// Lando environment settings
+if (getenv('LANDO_INFO')) {
+  $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+
+  // Files directory paths.
+  $settings['file_public_path'] = 'sites/default/files';
+  $settings['file_private_path'] = 'sites/default/files/private';
+
+  // This setting is specific to Lando.
+  $config_directories[CONFIG_SYNC_DIRECTORY] = '/app/config/common';
+
+  $settings['config_sync_directory'] = '/app/config/common';
+  $settings['file_temp_path'] = $_ENV['TEMP'];
+
+  // Generic hash salt for all local environments.
+  $settings['hash_salt'] = 'BfHE?EG)vJPa3uikBCZWW#ATbDLijMFRZgfkyayYcZYoy>eC7QhdG7qaB4hcm4x$';
+
+  // Allow any domains to access the site with Lando.
+  $settings['trusted_host_patterns'] = [
+    '^(.+)$',
+  ];
+
+  // Enable Configuration Read-only Mode (Only on Prod & UAT)
+  if (PHP_SAPI !== 'cli') {
+    $settings['config_readonly'] = TRUE;
+  }
+
+  // Add default config split settings for local development.
+  $config['config_split.config_split.local']['status'] = TRUE;
+  $config['config_split.config_split.dev']['status'] = FALSE;
+  $config['config_split.config_split.uat']['status'] = FALSE;
+  $config['config_split.config_split.prod']['status'] = FALSE;
+
+  $databases['default']['default'] = [
+    'database' => $lando_info['database']['creds']['database'],
+    'username' => $lando_info['database']['creds']['user'],
+    'password' => $lando_info['database']['creds']['password'],
+    'prefix' => '',
+    'host' => $lando_info['database']['internal_connection']['host'],
+    'port' => $lando_info['database']['internal_connection']['port'],
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+  ];
+
+  // Check for PHP Memcached libraries.
+  $memcache_exists = class_exists('Memcache', FALSE);
+  $memcached_exists = class_exists('Memcached', FALSE);
+  $memcache_module_is_present = file_exists(DRUPAL_ROOT . '/modules/contrib/memcache/memcache.services.yml');
+  if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
+    $settings['memcache']['servers'] = ['cache:11211' => 'default'];
+    $settings['memcache']['bins'] = ['default' => 'default'];
+    $settings['memcache']['key_prefix'] = 'leicageosystems_';
+
+    if (!drupal_installation_attempted()) {
+      $settings['cache']['default'] = 'cache.backend.memcache';
+    }
+  }
+}
